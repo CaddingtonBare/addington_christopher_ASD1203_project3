0426d0ebbb7e6658e08d95f29bfc143d8bf55267
Christopher Addington <caddington@fullsail.edu>
2012-03-14 01:13:02 -0400

    CouchDB AJAX call working!

--- asdcouch/asdcouch/app/_attachments/js/main.js
+++ asdcouch/asdcouch/app/_attachments/js/main.js
@@ -5,15 +5,6 @@
 
 $(function(){
 
-    var tmform = $('#teamform');
-    
-    tmform.validate({
-        invalidHandler: function(form, validator){},
-        submitHandler: function(){
-            var data = tmform.serializeArray();
-        }
-    });
-
     var toggleControls = function(n){
         var displayNone = { 'display': 'none' };
         var displayInline = { 'display': 'inline' };
@@ -225,25 +216,9 @@ $(function(){
             url: "_view/sport",
             dataType: "json",
             success: function(answer){
-                alert("JSON data retrieved successfully!");
-                for(var i=0, j=answer.teams.length; i<j; i++){
-                    var whichTeam = answer.teams[i];
-                    $(
-                        '<div>'+
-                            '<h3><img src="images/' + whichTeam.sports[1] + '_10px.png" />Basketball</h3>'+
-                            '<div>'+
-                                '<ul>'+
-                                    '<li>' + whichTeam.sports[0] + ' ' + whichTeam.sports[1] + '</li>'+
-                                    '<li>' + whichTeam.teamname[0] + ' ' + whichTeam.teamname[1] + '</li>'+
-                                    '<li>' + whichTeam.teamsize[0] + ' ' + whichTeam.teamsize[1] + '</li>'+
-                                    '<li>' + whichTeam.availabletime[0] + ' ' + whichTeam.availabletime[1] + '</li>'+
-                                    '<li>' + whichTeam.nextdate[0] + ' ' + whichTeam.nextdate[1] + '</li>'+
-                                    '<li>' + whichTeam.notes[0] + ' ' + whichTeam.notes[1] + '</li>'+
-                                '</ul>'+
-                            '</div>'+
-                        '</div>'
-                    ).appendTo('#jsontent');   
-                }
+            	$.each(answer.rows, function(index, sport){
+            		console.log(sport);
+            	});
             }
         });                    
     });
